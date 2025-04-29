23c4397be96ae8a5395e842dd483d6251759220a
alessandro massara <ale.ily.massara@gmal.com>
2025-04-28 21:26:25 -0300

    se implementó la imagen para el avatar

--- usuario/views.py
+++ usuario/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as django_login
 from usuario.forms import formulario_registro, formulario_editar_perfil
 from django.contrib.auth.decorators import login_required
+from usuario.models import info_extra
 
 def login(request):
     if request.method == "POST":
@@ -10,6 +11,7 @@ def login(request):
         if formulario.is_valid():
             usuario = formulario.get_user()
             django_login(request, usuario)
+            info_extra.objects.get_or_create(user = usuario)
             return redirect("inicio")
     else:
         formulario = AuthenticationForm()
@@ -27,11 +29,15 @@ def registro(request):
 
 @login_required
 def editar_perfil(request):
+    info_extra = request.user.info_extra
     if request.method == "POST":
-        formulario = formulario_editar_perfil(request.POST, instance=request.user)
+        formulario = formulario_editar_perfil(request.POST, request.FILES, instance=request.user)
         if formulario.is_valid():
+            if formulario.cleaned_data.get('avatar'):
+                info_extra.avatar = formulario.cleaned_data.get('avatar')
+            info_extra.save()
             formulario.save()
             return redirect("login")
     else:
-        formulario = formulario_editar_perfil(instance=request.user)
+        formulario = formulario_editar_perfil(initial={'avatar': info_extra.avatar}, instance=request.user)
     return render(request, "usuario/editar_perfil.html", {'formulario': formulario})
